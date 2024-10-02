063a567863a7c03b9c775ef33440a21df71515ea
Dliya Awliya Mufidah <awliyadliya@gmail.com>
2024-10-02 15:08:47 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -34,19 +34,19 @@ fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
  
 colors = ["#78281f", "#d74d42", "#f3584b", "#f1948a", "#fadbd8"]
  
-sns.barplot(x="recency", y="customer_id", data=rfm.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+sns.barplot(x="recency", y="customer_id", data=data_rfm.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel(None)
 ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
 ax[0].tick_params(axis ='x', labelsize=15)
  
-sns.barplot(x="frequency", y="customer_id", data=rfm.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+sns.barplot(x="frequency", y="customer_id", data=data_rfm.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
 ax[1].set_xlabel(None)
 ax[1].set_title("By Frequency", loc="center", fontsize=18)
 ax[1].tick_params(axis='x', labelsize=15)
  
-sns.barplot(x="monetary", y="customer_id", data=rfm.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+sns.barplot(x="monetary", y="customer_id", data=data_rfm.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
 ax[2].set_ylabel(None)
 ax[2].set_xlabel(None)
 ax[2].set_title("By Monetary", loc="center", fontsize=18)
