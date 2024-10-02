aba5e48e42e23658e89b91ca4fd92f50ab4888a3
Dliya Awliya Mufidah <awliyadliya@gmail.com>
2024-10-02 13:44:57 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -18,7 +18,8 @@ import seaborn as sns
 
 #membuat dataframe monthly orders
 def func_monthly_orders(data):
-    monthly_orders = data.groupby(['year_month', 'order_id_x']).agg({
+    monthly_orders = data.groupby('year_month').agg({
+        'order_id_x' = 'nunique'
         'price': 'sum'
     })
     monthly_orders = monthly_orders.reset_index()
