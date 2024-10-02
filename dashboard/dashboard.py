efc9b09d47b0a714002c70d6bfd56f80c2de8afa
Dliya Awliya Mufidah <awliyadliya@gmail.com>
2024-10-02 14:26:05 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -48,20 +48,20 @@ import seaborn as sns
 #     return by_state
 
 #membuat dataframe rfm analysis
-def func_rfm(df):
-    rfm = df.groupby(by="customer_id", as_index=False).agg({
-        "order_purchase_timestamp": "max", #mengambil tanggal order terakhir
-        "order_id_x": "nunique",
-        "price": "sum"
-    })
-    rfm.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+# def func_rfm(df):
+#     rfm = df.groupby(by="customer_id", as_index=False).agg({
+#         "order_purchase_timestamp": "max", #mengambil tanggal order terakhir
+#         "order_id_x": "nunique",
+#         "price": "sum"
+#     })
+#     rfm.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
     
-    rfm["max_order_timestamp"] = rfm["max_order_timestamp"].dt.date
-    recent_date = df["order_purchase_timestamp"].dt.date.max()
-    rfm["recency"] = rfm["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
-    rfm.drop("max_order_timestamp", axis=1, inplace=True)
+#     rfm["max_order_timestamp"] = rfm["max_order_timestamp"].dt.date
+#     recent_date = df["order_purchase_timestamp"].dt.date.max()
+#     rfm["recency"] = rfm["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+#     rfm.drop("max_order_timestamp", axis=1, inplace=True)
     
-    return rfm
+#     return rfm
 
 #memanggil main_data.csv
 url='https://drive.google.com/file/d/1DJniWK38Abf0ZVhA9T014nqjH3ZT1EY8/view?usp=sharing'
@@ -87,7 +87,7 @@ with st.sidebar:
 # monthly_orders = func_monthly_orders(main_data)
 # top_5_product = func_top_5_product(main_data)
 # by_state = func_by_state(main_data)
-rfm = func_rfm(main_data)
+# rfm = func_rfm(main_data)
 
 #MEMBUAT HEADER
 st.header("BRAZILIAN E-COMMERCE DASHBOARD")
@@ -172,30 +172,30 @@ st.header("BRAZILIAN E-COMMERCE DASHBOARD")
  
 
 #menambahkan visualisasi RFM
-st.subheader("Best Customer Based on RFM Parameters")
+# st.subheader("Best Customer Based on RFM Parameters")
  
-fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
+# fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
  
-colors = ["#78281f", "#d74d42", "#f3584b", "#f1948a", "#fadbd8"]
+# colors = ["#78281f", "#d74d42", "#f3584b", "#f1948a", "#fadbd8"]
  
-sns.barplot(x="recency", y="customer_id", data=rfm.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
-ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
-ax[0].tick_params(axis ='x', labelsize=15)
+# sns.barplot(x="recency", y="customer_id", data=rfm.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+# ax[0].set_ylabel(None)
+# ax[0].set_xlabel(None)
+# ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
+# ax[0].tick_params(axis ='x', labelsize=15)
  
-sns.barplot(x="frequency", y="customer_id", data=rfm.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
-ax[1].set_title("By Frequency", loc="center", fontsize=18)
-ax[1].tick_params(axis='x', labelsize=15)
+# sns.barplot(x="frequency", y="customer_id", data=rfm.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+# ax[1].set_ylabel(None)
+# ax[1].set_xlabel(None)
+# ax[1].set_title("By Frequency", loc="center", fontsize=18)
+# ax[1].tick_params(axis='x', labelsize=15)
  
-sns.barplot(x="monetary", y="customer_id", data=rfm.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
-ax[2].set_ylabel(None)
-ax[2].set_xlabel(None)
-ax[2].set_title("By Monetary", loc="center", fontsize=18)
-ax[2].tick_params(axis='x', labelsize=15)
+# sns.barplot(x="monetary", y="customer_id", data=rfm.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+# ax[2].set_ylabel(None)
+# ax[2].set_xlabel(None)
+# ax[2].set_title("By Monetary", loc="center", fontsize=18)
+# ax[2].tick_params(axis='x', labelsize=15)
  
-st.pyplot(fig)
+# st.pyplot(fig)
  
 st.caption('Bangkit Academy 2024 Batch 2')
